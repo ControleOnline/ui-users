b3ee4753eb75850e42b3efa0fa55c9f9924821c5
Luiz Kim <luiz.kim@controleonline.com>
2024-08-27 16:16:47 -0300

    Update

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,22 +1,28 @@
 <template>
-    <People :context="context" v-if="context"/>
-  </template>
-  
-  <script>
-  import People from "@controleonline/ui-people/src/components/People/Details.vue";
-  
-  export default {
-    components: {
-        People,
+  <People :context="context" :peopleId="this.user.people" v-if="context" />
+</template>
+
+<script>
+import People from "@controleonline/ui-people/src/components/People/Details.vue";
+
+export default {
+  components: {
+    People,
+  },
+
+  computed: {
+    user() {
+      let user = this.$store.getters["auth/user"] || {};
+      return user;
     },
-  
-    data() {
-      return {
-        context: 'profile'
-      };
-    },
-    created() {
-     },
-  };
-  </script>
-  
+  },
+  data() {
+    return {
+      context: "profile",
+    };
+  },
+  created() {
+
+  },
+};
+</script>
