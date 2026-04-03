a4f04fe99d690974caaa02fe30f1f2a89a59fc37
Luiz Kim <luiz.kim@controleonline.com>
2026-04-03 11:53:54 -0300

    Add summary state to store modules

--- src/store/users/index.js
+++ src/store/users/index.js
@@ -1,4 +1,4 @@
-import * as actions from "@controleonline/ui-default/src/store/default/actions";
+﻿import * as actions from "@controleonline/ui-default/src/store/default/actions";
 import * as getters from "@controleonline/ui-default/src/store/default/getters";
 import mutations from "@controleonline/ui-default/src/store/default/mutations";
 import Formatter from "@controleonline/ui-common/src/utils/formatter.js";
@@ -14,6 +14,7 @@ export default {
     error: "",
     violations: null,
     totalItems: 0,
+    summary: {},
     filters: {},
     columns: [
       {
